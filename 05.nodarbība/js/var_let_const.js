d08481a4af2689cf9c79da034cec1acd233db7f0
Indulis Straume <130855875+induliss@users.noreply.github.com>
2023-04-27 20:42:46 +0300

    27.04.23._20.42.

---
+++ 05.nodarbība/js/var_let_const.js
@@ -0,0 +1,30 @@
+console.log(
+    "Mainīgā dati vērtība - " + DATI + ", datu tips - " + typeof DATI
+  );
+
+  console.log("Izdruka pirms bloka, mainīgā a vērtība - " + a);
+  {
+    var a = 10;
+    console.log("Izdruka no bloka, mainīgā a vērtība - " + a);
+    {
+      const a = 20;
+      console.log("Izdruka no apakšbloka, mainīgā a vērtība - " + a);
+    }
+  }
+  a = a * a;
+  console.log("Izdruka pēc bloka, mainīgā a vērtība - " + a);
+
+  //console.log("Izdruka pirms bloka, mainīgā b vērtība - " + b);
+  {
+    let b = 10;
+    console.log(
+      "Izdruka no bloka (pirms apakšbloka), mainīgā b vērtība - " + b
+    );
+    {
+      let b = 30;
+      console.log("Izdruka no apakšbloka, mainīgā b vērtība - " + b);
+    }
+    console.log(
+      "Izdruka no bloka (pēc apakšbloka), mainīgā b vērtība - " + b
+    );
+  }
