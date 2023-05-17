67ee92d80e20705365458782d556a1b20fe91ef4
Īndulis Straume <induliss@gmail.com>
2023-05-17 13:58:15 +0300

    17.05.23.

---
+++ 08.nodarbība/js/oop.js
@@ -0,0 +1,6 @@
+const discrete_variable = 4;
+const array_example = ['a','b'];
+const function_example = function name_for_internal_use(){return x*x;};
+
+
+const car = {type:"Fiat", model:"500", color:"white"};
